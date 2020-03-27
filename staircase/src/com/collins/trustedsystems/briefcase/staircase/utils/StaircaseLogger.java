ace0b7b17c7b3423e5eb49f4a73cee495b69a111
Isaac <iamundson@gmail.com>
2020-03-26 23:39:13 -0500

    added logger for StairCASE

--- staircase/src/com/collins/trustedsystems/briefcase/staircase/utils/StaircaseLogger.java
+++ staircase/src/com/collins/trustedsystems/briefcase/staircase/utils/StaircaseLogger.java
@@ -1,26 +1,72 @@
 package com.collins.trustedsystems.briefcase.staircase.utils;
 
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleConstants;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.IConsoleView;
+import org.eclipse.ui.console.MessageConsole;
+import org.eclipse.ui.console.MessageConsoleStream;
+
 public class StaircaseLogger {
-	private static StringBuilder log = new StringBuilder();
+	private static MessageConsole console = findConsole("StairCASE");
+	private static MessageConsoleStream out = console.newMessageStream();
+	private static IConsoleView view = getConsoleView();
+
+	public static void logMessage(String message) {
+		out.println(message);
+		displayView();
+	}
+
+	public static void logInfo(String message) {
+		out.println("[INFO]: " + message);
+		displayView();
+	}
 
 	public static void logWarning(String message) {
-		log.append("[WARNING]: ");
-		log.append(message);
-		log.append(System.lineSeparator());
+		out.println("[WARNING]: " + message);
+		displayView();
 	}
 
 	public static void logError(String message) {
-		log.append("[ERROR]: ");
-		log.append(message);
-		log.append(System.lineSeparator());
+		out.println("[ERROR]: " + message);
+		displayView();
 	}
 
-	public static String getLog() {
-		return log.toString();
+	private static void displayView() {
+		if (view != null) {
+			view.display(console);
+		}
 	}
 
-	@Override
-	public String toString() {
-		return log.toString();
+	private static MessageConsole findConsole(String name) {
+		ConsolePlugin plugin = ConsolePlugin.getDefault();
+		IConsoleManager conMan = plugin.getConsoleManager();
+		IConsole[] existing = conMan.getConsoles();
+		for (int i = 0; i < existing.length; i++) {
+			if (name.equals(existing[i].getName())) {
+				return (MessageConsole) existing[i];
+			}
+		}
+		// no console found, so create a new one
+		MessageConsole myConsole = new MessageConsole(name, null);
+		conMan.addConsoles(new IConsole[] { myConsole });
+		return myConsole;
+	}
+
+	private static IConsoleView getConsoleView() {
+		IWorkbench wb = PlatformUI.getWorkbench();
+		IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
+		IWorkbenchPage page = win.getActivePage();
+		try {
+			return (IConsoleView) page.showView(IConsoleConstants.ID_CONSOLE_VIEW);
+		} catch (PartInitException e) {
+			return null;
+		}
 	}
 }
