6dbf6279ab9af08642ca374c6ce6d41c454b482e
Isaac Amundson <isaac.amundson@collins.com>
2022-03-23 15:08:23 -0500

    fixed name in activator

--- com.collins.trustedsystems.briefcase.modelaccess/src/com/collins/trustedsystems/briefcase/modelaccess/Activator.java
+++ com.collins.trustedsystems.briefcase.modelaccess/src/com/collins/trustedsystems/briefcase/modelaccess/Activator.java
@@ -9,11 +9,11 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "com.collins.trustedsystems.briefcase.getaadl"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "com.collins.trustedsystems.briefcase.modelaccess"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
-	
+
 	/**
 	 * The constructor
 	 */
